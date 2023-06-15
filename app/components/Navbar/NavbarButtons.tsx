dfe92d8938fcce127d49eb662a52aa7751d97c13
igortasic123 <63684263+igortasic123@users.noreply.github.com>
2023-06-15 15:57:22 -0400

    scroll

--- app/components/Navbar/NavbarButtons.tsx
+++ app/components/Navbar/NavbarButtons.tsx
@@ -2,13 +2,11 @@
 
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
+import { animateScroll as scroll } from "react-scroll";
+
 import { useState } from "react";
 
 import Link from "next/link";
-
-
-
-
 
 const NavbarButtons = () => {
   const [click, setclick] = useState(false);
@@ -17,29 +15,54 @@ const NavbarButtons = () => {
 
   const closeMobileMenu = () => setclick(false);
 
+  const scrollToHome = () => {
+    scroll.scrollTo(150);
+  };
+
+  const scrollToFeatures = () => {
+    scroll.scrollTo(1200);
+  };
+
+  const scrollToPricing = () => {
+    scroll.scrollTo(3900);
+  };
+
+  const scrollToContact = () => {
+    scroll.scrollTo(5700);
+  };
+
   return (
     <div className="flex flex-row gap-4 ">
+      <AiOutlineMenu
+        onClick={handleClick}
+        className="text-3xl text-gray-800 cursor-pointer sm:hidden"
+      />
 
-
-        <AiOutlineMenu
-          onClick={handleClick}
-          className="text-3xl text-gray-800 cursor-pointer sm:hidden"
-        />
-
-        {click && 
-        (
-          <div
-          className="flex flex-col  sm:hidden bg-gray-100 top-0 left-0 right-10  absolute
+      {click && (
+        <div
+          className="flex 
+          flex-col  
+          sm:hidden
+          bg-white
+          opacity-100
+         top-0 
+           left-0 
+           right-10  
+           absolute
           h-screen
           w-screen
           z-50
-          
+          scrollbar-hide
+          overflow-y-scroll
+          disable-scrollbars
+
+
          
-           " 
+           "
         >
-            <AiOutlineClose
-              onClick={handleClick}
-              className="text-3xl
+          <AiOutlineClose
+            onClick={handleClick}
+            className="text-3xl
               text-gray-800
               cursor-pointer
               justify-end
@@ -51,49 +74,50 @@ const NavbarButtons = () => {
 
               
               "
-            />
+          />
 
-
-            <div className="
+          <div
+            className="
             justify-center
             flex flex-col
             gap-10
             w-full
             h-full
             items-center
-            ">
-            <Link href="#">
+            "
+          >
+            <div onClick={scrollToHome}>
               <ul
                 onClick={closeMobileMenu}
                 className="cursor-pointer text-gray-800 font-semibold  duration-300  text-lg"
               >
                 Home
               </ul>
-            </Link>
-            <Link href="/Features">
+            </div>
+            <div>
               <ul
-                onClick={closeMobileMenu}
+                onClick={scrollToFeatures}
                 className="cursor-pointer text-gray-800 font-semibold transition duration-300  text-lg"
               >
                 Features
               </ul>
-            </Link>
-            <Link href="/Pricing">
+            </div>
+            <div>
               <ul
-                onClick={closeMobileMenu}
+                onClick={scrollToPricing}
                 className="cursor-pointer text-gray-800 font-semibold transition duration-300  text-lg"
               >
                 Pricing
               </ul>
-            </Link>
-            <Link href="/Contact">
+            </div>
+            <div>
               <ul
-                onClick={closeMobileMenu}
+                onClick={scrollToContact}
                 className="cursor-pointer text-gray-800 font-semibold transition duration-300  text-lg"
               >
                 Contact
               </ul>
-            </Link>
+            </div>
 
             <Link href="/Login">
               <ul
@@ -112,13 +136,9 @@ const NavbarButtons = () => {
                 Register
               </ul>
             </Link>
-            </div>
           </div>
-        )} 
-
-
-        
-                
+        </div>
+      )}
 
       <div className="sm:block hidden flex-row ">
         <Link
