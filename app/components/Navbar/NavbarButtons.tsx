b897745934ff87379e98a97fd67f913acbb45bf5
igortasic123 <63684263+igortasic123@users.noreply.github.com>
2023-06-15 15:45:45 -0400

    navbar

--- app/components/Navbar/NavbarButtons.tsx
+++ app/components/Navbar/NavbarButtons.tsx
@@ -1,28 +1,129 @@
 "use client";
 
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 import { useState } from "react";
 
 import Link from "next/link";
 
 
+
+
+
 const NavbarButtons = () => {
-  const [menu, setMenu] = useState(true);
+  const [click, setclick] = useState(false);
+
+  const handleClick = () => setclick(!click);
+
+  const closeMobileMenu = () => setclick(false);
 
   return (
-    <div className="flex flex-row gap-4">
-      <div className="sm:hidden">
+    <div className="flex flex-row gap-4 ">
+
+
         <AiOutlineMenu
-          onClick={() => setMenu(!menu)}
-          className="text-3xl cursor-pointer  "
+          onClick={handleClick}
+          className="text-3xl text-gray-800 cursor-pointer sm:hidden"
         />
-      </div>
+
+        {click && 
+        (
+          <div
+          className="flex flex-col  sm:hidden bg-gray-100 top-0 left-0 right-10  absolute
+          h-screen
+          w-screen
+          z-50
+          
+         
+           " 
+        >
+            <AiOutlineClose
+              onClick={handleClick}
+              className="text-3xl
+              text-gray-800
+              cursor-pointer
+              justify-end
+              self-end
+              mr-5
+              mt-10
+  
+
+
+              
+              "
+            />
+
+
+            <div className="
+            justify-center
+            flex flex-col
+            gap-10
+            w-full
+            h-full
+            items-center
+            ">
+            <Link href="#">
+              <ul
+                onClick={closeMobileMenu}
+                className="cursor-pointer text-gray-800 font-semibold  duration-300  text-lg"
+              >
+                Home
+              </ul>
+            </Link>
+            <Link href="/Features">
+              <ul
+                onClick={closeMobileMenu}
+                className="cursor-pointer text-gray-800 font-semibold transition duration-300  text-lg"
+              >
+                Features
+              </ul>
+            </Link>
+            <Link href="/Pricing">
+              <ul
+                onClick={closeMobileMenu}
+                className="cursor-pointer text-gray-800 font-semibold transition duration-300  text-lg"
+              >
+                Pricing
+              </ul>
+            </Link>
+            <Link href="/Contact">
+              <ul
+                onClick={closeMobileMenu}
+                className="cursor-pointer text-gray-800 font-semibold transition duration-300  text-lg"
+              >
+                Contact
+              </ul>
+            </Link>
+
+            <Link href="/Login">
+              <ul
+                onClick={closeMobileMenu}
+                className="cursor-pointer text-gray-800 font-semibold transition duration-300  text-lg"
+              >
+                Login
+              </ul>
+            </Link>
+
+            <Link href="/SignUp">
+              <ul
+                onClick={closeMobileMenu}
+                className="cursor-pointer text-gray-800 font-semibold transition duration-300  text-lg"
+              >
+                Register
+              </ul>
+            </Link>
+            </div>
+          </div>
+        )} 
+
+
+        
+                
 
       <div className="sm:block hidden flex-row ">
         <Link
           className="
-      cursor-pointer
+          cursor-pointer
       py-2 px-4 text-gray-800 font-semibold transition duration-300  text-lg"
           href="/Login"
         >
