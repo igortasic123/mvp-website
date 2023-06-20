ff827dd077c6c195cb3ca14bd7db6655efdbf345
igortasic123 <63684263+igortasic123@users.noreply.github.com>
2023-06-20 11:43:10 -0400

    demo

--- app/Demo/page.tsx
+++ app/Demo/page.tsx
@@ -1,15 +1,28 @@
 
 'use client'
 
+import Link from "next/link";
+import { InlineWidget } from "react-calendly";
+import { LuBird } from "react-icons/lu";
+
 
 const Demo = () => {
 
 
 
     return (
-        <div>
-            Hey 
+      <div className="h-screen justify-center items-center bg-blue-100">
+        <div className="flex flex-col justify-center items-center pt-10">
+        <Link href="/">
+          <LuBird className="text-6xl " />
+        </Link>
+          <h1 className="text-3xl font-bold text-center p-5">Demo</h1>
+          <p className="text-center">
+            Schedule a demo with us to see how we can help your business.
+          </p>
         </div>
+      <InlineWidget url="https://calendly.com/birdsoftware/30min" />
+    </div>
 
     );
 }
